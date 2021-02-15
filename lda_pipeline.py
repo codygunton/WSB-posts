65bc9b07b96cb820ecec1220505d34e526cf88c4
Cody Gunton <codygunton@gmail.com>
2021-02-15 11:24:23 -0500

    Started systematic development of pipeline.

--- lda_pipeline.py
+++ lda_pipeline.py
@@ -7,16 +7,16 @@ from sparknlp.annotator import (Tokenizer,
                                 PerceptronModel)
 from pyspark.ml import Pipeline
 
-from spacy.lang.en.stop_words import STOP_WORDS
-
-STOP_WORDS = list(STOP_WORDS)
+# from spacy.lang.en.stop_words import STOP_WORDS
+# STOP_WORDS = list(STOP_WORDS)
+# print(STOP_WORDS)
 
 
 def build_pipeline():
     assembler = (
         DocumentAssembler()
-        .setInputCol("text")
-        .setOutputCol("document")
+        .setInputCol('text')
+        .setOutputCol('document')
     )
 
     tokenizer = (
@@ -25,57 +25,69 @@ def build_pipeline():
         .setOutputCol('tokenized')
     )
 
+    stopwords_cleaner = (
+        StopWordsCleaner.pretrained("stopwords_en", "en")
+        .setInputCols(['tokenized'])
+        .setOutputCol('cleaned')
+        .setCaseSensitive(False)
+    )
+
+    # stopwords_cleaner = (
+    #     StopWordsCleaner()
+    #     .setInputCols(['tokenized'])
+    #     .setOutputCol('cleaned')
+    #     .setStopWords(STOP_WORDS)
+    #     .setCaseSensitive(False)
+    # )
+
+    lemmatizer = (
+        LemmatizerModel.pretrained()
+        .setInputCols(['cleaned'])
+        .setOutputCol('lemmatized')
+    )
+
     normalizer = (
         Normalizer()
-        .setInputCols(['tokenized'])
+        .setInputCols(['lemmatized'])
         .setOutputCol('normalized')
         .setLowercase(True)
         .setCleanupPatterns(['[^A-Za-z]',
                              'http.*'])
     )
 
-    lemmatizer = (
-        LemmatizerModel.pretrained()
-        .setInputCols(['normalized'])
-        .setOutputCol('lemmatized')
-    )
-
-    stopwords_cleaner = (
-        StopWordsCleaner()
-        .setInputCols(['lemmatized'])
-        .setOutputCol('unigrams')
-        .setStopWords(STOP_WORDS)
-    )
-
     # ngrammer = (
     #     NGramGenerator()
-    #     .setInputCols(['lemmatized'])
+    #     .setInputCols([''])
     #     .setOutputCol('ngrams')
     #     .setN(3)
     #     .setEnableCumulative(True)
     #     .setDelimiter('_')
     # )
 
-    pos_tagger = (
-        PerceptronModel.pretrained('pos_anc')
-        .setInputCols(['document', 'lemmatized'])
-        .setOutputCol('pos')
-    )
+    # pos_tagger = (
+    #     PerceptronModel.pretrained('pos_anc')
+    #     .setInputCols(['document', 'lemmatized'])
+    #     .setOutputCol('pos')
+    # )
 
     finisher = (
         Finisher()
-        .setInputCols(['unigrams',
-                       'pos'])
+        .setInputCols(['normalized',
+                       # 'pos'
+                       ])
     )
 
     pipeline = (Pipeline()
                 .setStages([assembler,
                             tokenizer,
-                            normalizer,
-                            lemmatizer,
                             stopwords_cleaner,
-                            pos_tagger,
+                            lemmatizer,
+                            normalizer,
+                            # pos_tagger,
                             # ngrammer,
                             finisher]))
+
+    # to do: try LightPipeline as in
+    # https://nlp.johnsnowlabs.com/docs/en/concepts#lightpipeline
 
     return pipeline
