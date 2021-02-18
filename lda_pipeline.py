01ff47d379a25754bba7d542fbb0bb09283e0693
Cody Gunton <codygunton@gmail.com>
2021-02-17 22:19:46 -0500

    Added pipeline support for keeping emojis.

--- lda_pipeline.py
+++ lda_pipeline.py
@@ -19,6 +19,7 @@ assembler = DocumentAssembler()
 
 tokenizer = Tokenizer()
 # add ['‘', '’', '“', '”'] as context characters
+# doing this in a verbose way because it may be clarifying.
 char_names = ['LEFT SINGLE QUOTATION MARK',
               'RIGHT SINGLE QUOTATION MARK',
               'LEFT DOUBLE QUOTATION MARK',
@@ -46,10 +47,33 @@ stopwords_cleaner.setStopWords(stopwords)
 lemmatizer = LemmatizerModel.pretrained()
 
 
+# this does not keep all emojis, but it keeps a lot of them.
+# for instance, it does not distinguish race, but it has enough
+# characters to express the Becky emoji.
+keeper_regex = ''.join([
+    '[^0-9A-Za-z$&%=',
+    # special characters for Becky
+    '\u200d\u2640\u2641\u26A5\ufe0f',
+    # ranges covering all emojis expressible
+    # using only one unicode character.
+    '\U0001f300-\U0001f321',
+    '\U0001f324-\U0001f393',
+    '\U0001f39e-\U0001f3f0',
+    '\U0001f400-\U0001f4fd',
+    '\U0001f4ff-\U0001f53d',
+    '\U0001f550-\U0001f567',
+    '\U0001f5fa-\U0001f64f',
+    '\U0001f680-\U0001f6c5',
+    '\U0001f90c-\U0001f93a',
+    '\U0001f947-\U0001f978',
+    '\U0001f97a-\U0001f9cb',
+    '\U0001f9cd-\U0001f9ff',
+    '\U0001fa90-\U0001faa8]'])
+
 normalizer = (
     Normalizer()
     .setLowercase(True)
-    .setCleanupPatterns(['[^0-9A-Za-z$&%]',
+    .setCleanupPatterns([keeper_regex,
                          'http.*'])
 )
 
