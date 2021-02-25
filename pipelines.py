7963e93f8b714eaa51921f24838b725c689df22f
Cody Gunton <codygunton@gmail.com>
2021-02-24 20:59:51 -0500

    Implemented additonal tokenization in n-grams. Cleaned up.

--- pipelines.py
+++ pipelines.py
@@ -1,13 +1,17 @@
 from sparknlp.base import (DocumentAssembler,
-                           Finisher)
+                           Finisher,
+                           Chunk2Doc)
 
 from sparknlp.annotator import *
 from pyspark.ml import Pipeline
-import unicodedata
+from pyspark.sql import types as T
+import pyspark.sql.functions as F
 
 from download_pretrained import PretrainedCacheManager
 
-emoji_ranges = [
+emojis_regex = (
+    '[' + 
+    "".join([
         # special characters for Becky
         '\u200d\u2640\u2641\u26A5\ufe0f',
         # ranges covering all emojis expressible
@@ -25,17 +29,37 @@ emoji_ranges = [
         '\U0001f97a-\U0001f9cb',
         '\U0001f9cd-\U0001f9ff',
         '\U0001fa90-\U0001faa8'
-]
+    ])
+    + 
+    ']'
+)
 
 
 def write_emoji_matcher_rules():
     # generate matcher rules
-    emojis = ''.join(emoji_ranges)
-    # regex = f"[{emojis}|{emojis}\s+]+"
-    regex = f"[{emojis}]"
-    matcher_rule = regex+"~emojis"
+    matcher_rule = emojis_regex+"~emojis"
     with open("emoji_matcher_rules.csv", "w") as fobj:
         fobj.writelines([matcher_rule])
+        
+        
+
+def preprocess_texts(df):
+    texts = (
+        # to do: find uniform way of preprocessing usingn pure Spark
+        #        can only find convoluted ways in Spark NLP.
+        #        Is using a UDF slower?
+        df.withColumn("text_no_emojis",
+                      F.regexp_replace("text",
+                                       emojis_regex, " "))  # replacing with "" is bad
+        .withColumn("text_no_emojis", 
+                      F.regexp_replace("text_no_emojis", "[“”]", "\""))
+        .withColumn("text_no_emojis", 
+                    F.regexp_replace("text_no_emojis", "[‘’]", "\'"))
+        # to keep positions of emojis (not necessary, currently)
+        .select(["text", "text_no_emojis"])
+    )    
+    return texts
+
 
 
 def get_lda_components():
@@ -62,7 +86,6 @@ def get_lda_components():
         .setLowercase(True)
     )
     # this does not keep all emojis, but it keeps a lot of them.
-    # for instance, it does not distinguish skin color, but it has
     # enough characters to express the Becky emoji.
     keeper_regex = ''.join(['[^0-9A-Za-z\$\&%\.,\?!\'‘’\"“”]'])
     document_normalizer.setPatterns([keeper_regex,
@@ -71,19 +94,31 @@ def get_lda_components():
     # get sentence detector. This is  used for POS tagging.
     sentence_detector = SentenceDetector()
 
-    # build tokenier
+    # build tokeniers
+    tokenizer_infix_patterns = [
+        # didn't try to be exhaustive here.
+        '(\w+)(n\'t)',    # split wasn't |-> was  n't
+        '(\w+)(\'m)',     #          I'm |-> I     'm      
+        '(\w+)(\'s)',     #        she's |-> she   's
+        '(\w+)(\'d)',     #           'd |->       'd
+        '(\w+)(\'ve)',     #          've |->       've
+        '(\w+)(\'re)'     #          've |->       've      
+    ]
+    
     tokenizer = (
         Tokenizer()
-        .setInfixPatterns(  # didn't try to be exhaustive here.
-        ['(\w+)(n\'t)',     # split wasn't |-> was n't
-         '(\w+)(\'m)',      #         I'm  |-> I 'm      
-         '(\w+)(\'s)'       #       she's  |-> she 's
-         ])
+        .setInfixPatterns(tokenizer_infix_patterns)
+    )
+
+    tokenizer2 = (
+        Tokenizer()
+        .setInfixPatterns(tokenizer_infix_patterns)
     )
     
     # build lemmatizer. dict from:
     #  https://raw.githubusercontent.com/mahavivo/\
     #          vocabulary/master/lemmas/AntBNC_lemmas_ver_001.txt
+    # I think it's the same as the pretrained, but inspectible.
     lemmatizer = (
         Lemmatizer()
         .setDictionary("./AntBNC_lemmas_ver_001.txt",
@@ -120,8 +155,6 @@ def get_lda_components():
 
 
     # choose which sequences of POS tags can be used to generate n-grams.
-    # I choose a large list for purposes of illustraiton, but I would advocate
-    # to be more focused in practice, depending on particular objectives.
     chunker = (
         Chunker()
         .setRegexParsers(['<JJ>+<NN>',            # adjective-noun
@@ -129,6 +162,16 @@ def get_lda_components():
                           '<MD>+<RB>*<VB>',       # [should, not, sell]
                           '<VBP>*<RB>*<VB>+<NN>'  # [do*, not*, sell, gme]
                           ])
+    )
+
+
+    # convert POS chunks back to docs
+    chunk2doc = Chunk2Doc()
+
+    # tokenize again
+    tokenizer2 = (
+        Tokenizer()
+        .setInfixPatterns(tokenizer_infix_patterns)
     )
     
     # build finisher
@@ -146,11 +189,63 @@ def get_lda_components():
             ngrammer,
             pos_tagger,
             chunker,
+            chunk2doc,
+            tokenizer2,
             finisher)
 
+def lda_preproc_finisher(df):
+    # TODO: rewrite to this a class extending Transfromer as in SO51415784.
+    # and then include as a pipeline component.
+    #
+    # "unigram" column type is an array of struct, where each struct
+    # records (among other things) a token and the "sentence_id",
+    # which appears to record the chunk from which the token came.
+    # (recall that the chunks are ngrams that we have cleaned and
+    # are now reassembling).
+
+    # outline:
+    #  - explode array in unigrams column into rows.
+    #  - expand each struct into columns. keep only token and sentence id
+    #  - concatenate unigrams to n-grams, groubying by sentence id.
+    #  - aggregate n-grams into arrays, grouping by text.
+
+    # will only need these columns to build finished n-grams
+    
+    output = (df
+              .select("text", "unigrams")
+              # explode unigrams columsn into rows
+              .withColumn("exploded", 
+                          F.explode(df['unigrams']))
+              # select only columns of intereest
+              .select("text", "exploded.result", "exploded.metadata"))
+
+    output = (output
+              .select("text",
+                      output["result"].alias("unigram"),
+                      # metadata is a map as in sentence -> 0 (or 1, 2,...)
+                      # just keep the value
+                      F.element_at(output.metadata, 'sentence')
+                      .alias("sentence"))
+              # concatenate unigrams to ngrams
+              .groupby('sentence', 'text')
+              .agg(F.concat_ws("_", F.collect_list('unigram'))
+                    .alias("ngram"))
+              # collect ngrams into arrays, one for each text
+              .groupby('text')
+              .agg(F.collect_list('ngram')
+                    .alias("finished_ngrams")))
+    
+    # adjoin emojis
+    emoji_finisher = Finisher().setInputCols("emojis")
+    emoji_df = emoji_finisher.transform(df)
+    output = (output
+              .join(emoji_df.select("text", "finished_emojis"), 
+                    "text"))
+    
+    return output
 
 # bag of words bag of emojis pipeline
-def build_lda_pipeline(pipeline_components=None):
+def build_lda_preproc_pipeline(pipeline_components=None):
     # get_pipeline_components
     if not pipeline_components:
         _ = get_lda_components()
@@ -169,6 +264,8 @@ def build_lda_pipeline(pipeline_components=None):
      ngrammer,
      pos_tagger,
      chunker,
+     chunk2doc,
+     tokenizer2,
      finisher) = _
 
     # assemble the pipeline
@@ -198,43 +295,31 @@ def build_lda_pipeline(pipeline_components=None):
      .setInputCols(['normalized_document'])
      .setOutputCol('tokenized'))
 
+    (pos_tagger
+     .setInputCols(['sentences', 'tokenized'])
+     .setOutputCol('pos_tags'))
+    
+    (chunker
+     .setInputCols(['sentences', 'pos_tags'])
+     .setOutputCol('dirty_ngrams'))
+
+    (chunk2doc
+     .setInputCols(['dirty_ngrams'])
+     .setOutputCol('doc_chunks'))
+
+    (tokenizer2
+     .setInputCols(['doc_chunks'])
+     .setOutputCol('retokenized'))
+    
     (lemmatizer
-     .setInputCols(['tokenized'])
+     .setInputCols(['retokenized'])
      .setOutputCol('lemmatized'))
 
     (normalizer
      .setInputCols(['lemmatized'])
      .setOutputCol('unigrams'))
 
-    (stopwords_cleaner
-     .setInputCols(['unigrams'])
-     .setOutputCol('cleaned_unigrams'))
     
-    
-    (ngrammer
-     .setInputCols(['cleaned_unigrams'])
-     .setOutputCol('naive_ngrams'))
-    
-    (pos_tagger
-     .setInputCols(['sentences', 'unigrams'])
-     .setOutputCol('pos_tags'))
-    
-    (chunker
-     .setInputCols(['sentences', 'pos_tags'])
-     .setOutputCol('ngrams')
-    )
-
-    (finisher
-     .setInputCols(['tokenized',
-                    'cleaned_unigrams',
-                    'lemmatized',
-                    'emojis', 
-                    'unigrams', 
-                    'naive_ngrams',
-                    'pos_tags',
-                    'ngrams'
-                   ]))
-
     pipeline = (Pipeline()
                 .setStages([assembler,
                             emoji_matcher,
@@ -242,19 +327,20 @@ def build_lda_pipeline(pipeline_components=None):
                             document_normalizer,
                             sentence_detector,
                             tokenizer,
-                            lemmatizer,
-                            normalizer,
-                            stopwords_cleaner,
-                            ngrammer,
                             pos_tagger,
                             chunker,
-                            finisher]))
+                            chunk2doc,
+                            tokenizer2,
+                            lemmatizer,
+                            normalizer
+                           ]))
+
 
     return pipeline
 
 
 # Pipeline for cleaning text before processing with pretrained embeddings.
-
+# This is under construction.
 def get_embedding_preproc_components():
     # get cache mananager to avoid repeated downloads
     cache_manager = PretrainedCacheManager()
@@ -349,10 +435,4 @@ def build_embedding_preproc_pipeline(pipeline_components=None):
 #                             finisher
                             ]))
 
-    # to do: try LightPipeline as in
-    # https://nlp.johnsnowlabs.com/docs/en/concepts#lightpipeline
-
     return pipeline
-
-
-
