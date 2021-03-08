d85006c1021458c01b58de1f33fbe8f2b6ef4112
Cody Gunton <codygunton@gmail.com>
2021-03-08 18:12:03 -0500

    Cleaned up to reflect streamelined preprocessing.

--- embedding_pipeline.py
+++ embedding_pipeline.py
@@ -1,46 +1,12 @@
 from sparknlp.base import (DocumentAssembler,
-                           Finisher,
-                           Chunk2Doc)
+                           Finisher)
 
 from sparknlp.annotator import *
 from pyspark.ml import Pipeline
 from pyspark.sql import types as T
 import pyspark.sql.functions as F
 
-from download_pretrained import PretrainedCacheManager
-
-emojis_regex = ( 
-    # first the Becky emoji
-    '\U0001f471\u200d\u2640\ufe0f|' +
-    '[' +
-    "".join([
-        # ranges covering all emojis expressible
-        # using only one unicode character.
-        '\U0001f300-\U0001f321',
-        '\U0001f324-\U0001f393',
-        '\U0001f39e-\U0001f3f0',
-        '\U0001f400-\U0001f4fd',
-        '\U0001f4ff-\U0001f53d',
-        '\U0001f550-\U0001f567',
-        '\U0001f5fa-\U0001f64f',
-        '\U0001f680-\U0001f6c5',
-        '\U0001f90c-\U0001f93a',
-        '\U0001f947-\U0001f978',
-        '\U0001f97a-\U0001f9cb',
-        '\U0001f9cd-\U0001f9ff',
-        '\U0001fa90-\U0001faa8'
-    ])
-    + 
-    ']'
-)
-
-
-def write_emoji_matcher_rules():
-    # generate matcher rules
-    matcher_rule = emojis_regex+"~emojis"
-    with open("emoji_matcher_rules.csv", "w") as fobj:
-        fobj.writelines([matcher_rule])
-        
+from download_pretrained import PretrainedCacheManager      
         
 
 def preprocess_texts(df):
@@ -64,9 +30,9 @@ def preprocess_texts(df):
         # space and periods.
         .withColumn("text",
                       F.regexp_replace("text",
-                                       emojis_regex
-                                       + "|[\\n\\r]|\\- |[\.]{2,}",
+                                       "[\\n\\r]|\\- |[\.]{2,}",
                                        ". "))
+        # could probably combine the next two operations
         .withColumn("text", 
                       F.regexp_replace("text", "[“”]", "\""))
         .withColumn("text", 
@@ -90,7 +56,7 @@ def get_embedding_preproc_components():
 
     # get document assemblers
     assembler = DocumentAssembler()
-    # note: shrink cleanup mode seems to lose some sentnces
+    # note: shrink cleanup mode seems to lose some sentences
     # anyway, sentencer seems to make document cleanup redundant
 
     # get sentence detector
@@ -100,21 +66,13 @@ def get_embedding_preproc_components():
         .setMinLength(3)
         .setExplodeSentences(True)
     )
-    #.setExplodeSentences(True)
 
     # build normalizer
-    document_normalizer = (
-        DocumentNormalizer()
-        # .setLowercase(True)
-    )
-    # this does not keep all emojis, but it keeps a lot of them.
-    # for instance, it does not distinguish skin color, but it has
-    # enough characters to express the Becky emoji.
+    document_normalizer = DocumentNormalizer()
+    # this is inefficient now--could go in the earlier preprocessing.
+    # will leave it for now to avoid accidentally breaking anything.
     keeper_regex = ''.join(['[^0-9A-Za-z\$\&%\.,\?!\'‘’\"“”]'])
-    document_normalizer.setPatterns([keeper_regex,
-                                     # 'http\S+', 
-                                     # '\&*\#*x200B\S*'
-                                     ])
+    document_normalizer.setPatterns([keeper_regex])
 
     
     # build finisher
@@ -135,7 +93,6 @@ def build_embedding_preproc_pipeline(pipeline_components=None):
     (assembler,
      sentence_detector,
      document_normalizer,
-     # USE,
      finisher
     ) = _
 
@@ -159,43 +116,7 @@ def build_embedding_preproc_pipeline(pipeline_components=None):
                 .setStages([assembler,
                             document_normalizer,
                             sentence_detector,
-#                             USE, 
                             finisher
                             ]))
 
     return pipeline
-
-
-def embedding_preproc_finisher(df):    
-    output = (df
-              .select("text", "unigrams")
-              # explode unigrams columsn into rows
-              .withColumn("exploded", 
-                          F.explode(df['unigrams']))
-              # select only columns of intereest
-              .select("text", "exploded.result", "exploded.metadata"))
-
-    output = (output
-              .select("text",
-                      output["result"].alias("unigram"),
-                      # metadata is a map as in sentence -> 0 (or 1, 2,...)
-                      # just keep the value
-                      F.element_at(output.metadata, 'sentence')
-                      .alias("sentence"))
-              # concatenate unigrams to ngrams
-              .groupby('sentence', 'text')
-              .agg(F.concat_ws("_", F.collect_list('unigram'))
-                    .alias("ngram"))
-              # collect ngrams into arrays, one for each text
-              .groupby('text')
-              .agg(F.collect_list('ngram')
-                    .alias("finished_ngrams")))
-    
-    # adjoin emojis
-    emoji_finisher = Finisher().setInputCols("emojis")
-    emoji_df = emoji_finisher.transform(df)
-    output = (output
-              .join(emoji_df.select("text", "finished_emojis"), 
-                    "text"))
-    
-    return output
